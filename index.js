59b8423a2cf4dab6a6ef70dec6082167801e4fae
Thiago Xanhenha <tjbarbosadev@gmail.com>
2022-12-20 18:19:16 -0300

    configuring caporal

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
 
-chokidar.watch('.')
-  .on('add', () => console.log('FILE ADDED'))
-  .on('change', () => console.log('FILE CHANGED'))
-  .on('unlink', () => console.log('FILE UNLINKED'))
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action( async ({filename}) => {
+    const name = filename || 'index.js';
+
+    try {
+      await fs.promises.access(name);
+    } catch(err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    const start = debounce(() => {
+      console.log('Starting users program');
+    }, 100);
+
+    chokidar.watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
